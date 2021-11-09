da3d129f4a22a084e8a72d31cb845d91e4c6ebe5
aras3640 <aras3640@gmail.com>
2021-11-09 19:51:34 +0900

    Add text file[hw.5]

---
+++ project5/project5/hw5.c
@@ -0,0 +1,47 @@
+#include<stdio.h> 
+
+
+int even(int a);
+int odd(int a); 
+
+int even(int a)                
+{
+    if (a % 2 == 0)                 
+        printf("%d", a);        
+    return 0;
+}
+
+int odd(int a)                 
+{
+    if (a % 2 == 1)                 
+        printf("%d", a);        
+    return 0;
+}
+
+
+int main()
+{
+    int i;
+    int a[5] = { 0 };            
+
+    printf("5개의 정수를 입력:\n");      
+
+    for (i = 0; i < 5; i++)                
+    {
+        scanf_s("%d", &a[i]);             
+    }
+
+    printf("짝수 출력:");                 
+    for (i = 0; i < 5; i++)               
+    {
+        even(a[i]);                   
+    }
+
+    printf("홀수 출력:");
+    for (i = 0; i < 5; i++)
+    {
+        odd(a[i]);                    
+    }
+
+
+}
